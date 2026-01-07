8a04ed97c8ab38bf7088da8f6aa327dd43496462
kenisonm-osu <kenisonm@oregonstate.edu>
2026-01-07 14:21:41 -0800

    1/7

---
+++ python-basics/hello.py
@@ -0,0 +1,5 @@
+def main() -> None:
+    print("Hello world!", 3.14)
+
+if __name__ == "__main__":
+    main()
---
+++ python-basics/variables.py
@@ -0,0 +1,26 @@
+def main() -> None:
+    #print(3)
+    #print(3.14)
+    # comment starts with a # character
+    #print(True)
+    #print(False)
+    #print("hello")
+    #print(8j)
+    #print(None)
+    math()
+
+def math() -> None:
+    print(3 + 3)
+    print(3 - 3)
+    print(3 * 3)
+    print(3 / 3)
+    print(3 / 2)
+    print(3 // 2)
+    print(6 % 3)
+    print(7 % 3)
+    print(8 % 3)
+    print(9 % 3)
+    print(2 ** 5)
+
+if __name__ == "__main__":
+    main()
